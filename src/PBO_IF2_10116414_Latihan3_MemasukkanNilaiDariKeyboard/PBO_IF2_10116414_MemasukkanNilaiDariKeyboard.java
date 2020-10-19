cebd0d0e2d1bb46353c743744ee5f363e2e2432b
sweenutt <sweenutt@192.168.3.19>
2020-10-20 01:34:50 +0700

    Upload Latihan 1 Biodata

---
+++ src/PBO_IF2_10116414_Latihan3_MemasukkanNilaiDariKeyboard/PBO_IF2_10116414_MemasukkanNilaiDariKeyboard.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_IF2_10116414_Latihan3_MemasukkanNilaiDariKeyboard;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Memasukkan Nilai DariKeyboard
+ */
+public class PBO_IF2_10116414_MemasukkanNilaiDariKeyboard {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Masukan Nama Anda : ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama Anda Adalah " +nama);
+        
+    }
+}
